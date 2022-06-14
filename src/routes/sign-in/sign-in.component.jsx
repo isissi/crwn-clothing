8a6874cd7ad37f92cd4ea4ff87bd85892c09d9ab
isissi <isissi_525@hotmail.com>
2022-06-14 14:08:33 -0600

    add google sign in feature to sign in pgae

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,17 @@
-
+import { signInWithGooglePopup } from "../../utils/firebase.utils";
 
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const res = await signInWithGooglePopup();
+    console.log(res);
+  }
+
   return (
     <div>
-      Sign In
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>
+        Sign In
+      </button>
     </div>
   )
 }
--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,3 +13,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider(); 
+
+provider.setCustomParameters({
+  prompt: 'select_account'
+})
+
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
