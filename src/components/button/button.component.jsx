3b3163835db6832a4ba5956cb1dfc6efa3b2ccff
isissi <isissi_525@hotmail.com>
2022-06-20 14:52:40 -0600

    fix button types typo

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,13 +1,13 @@
 import './button.styles.scss'
 
-const BUTTON_TYOES_CLASSES = {
-  giigke: 'google-sign-in', 
+const BUTTON_TYPES_CLASSES = {
+  google: 'google-sign-in', 
   interted: 'inverted'
 }
 
 const Button = ({ children, buttonType, ...otherProps }) => {
   return (
-    <div className={`button-container ${BUTTON_TYOES_CLASSES[buttonType]}`} {...otherProps}>
+    <div className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`} {...otherProps}>
       {children}
     </div>
   )
