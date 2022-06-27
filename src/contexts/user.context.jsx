3f559832817881354b1872ab1a7f88f2ebc4a184
isissi <isissi_525@hotmail.com>
2022-06-27 13:07:53 -0600

    add createUserDocumentFromAuth to navigation.jsx

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react"; 
-import { onAuthStateChangedListener } from "../utils/firebase.utils";
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase.utils";
 
 export const UserContext = createContext({
   currentUser: null,
@@ -12,6 +12,9 @@ export const UserProvider = ({children}) => {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
       setCurrentUser(user);
     });
 
