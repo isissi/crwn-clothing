ea767ac64b13ad22009b90ecbfc2b35645fdd7fa
isissi <isissi_525@hotmail.com>
2022-06-28 19:51:40 -0600

    import cartContext to index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.scss';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.contex';
+import { CartContext } from './contexts/cart.context';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,7 +14,9 @@ root.render(
     <BrowserRouter>
     <UserProvider>
       <ProductsProvider>
-        <App />
+        <CartContext>
+          <App />
+        </CartContext>
       </ProductsProvider>
     </UserProvider>
     </BrowserRouter>
