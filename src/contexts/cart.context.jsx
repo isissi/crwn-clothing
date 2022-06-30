f57da28f0324ee0372be92c23ab13e98f1eb8e4d
isissi <isissi_525@hotmail.com>
2022-06-29 18:42:39 -0600

    add helper function addCartItem

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,35 @@
 import { createContext, useState } from 'react';
 
+const addCartItem = (cartItems, productsToAdd) => {
+  const existingCartItem = cartItems.find(cartItem => cartItem.id === productsToAdd.id); 
+
+  //increment quantity if the product is found in cart
+  if (existingCartItem) {
+    return cartItems.map(cartItem => cartItem.id === productsToAdd.id ? 
+      {...cartItem, quantity: cartItem.quantity ++} 
+      : cartItem
+    );
+  }
+  
+  return [...cartItems, { ...productsToAdd, quantity: 1 }];
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsOpen: () => {},
+  cartItems: [], 
+  addItemToCart: () => {}
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productsToAdd) => {
+    setCartItems(addCartItem(cartItems, productsToAdd));
+  }
+
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
 
   return (
     <CartContext.Provider value={value}>{children}</CartContext.Provider>
