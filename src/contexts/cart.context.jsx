8c67f250eac45ad104bfa654fd12cf9ab9b3ee65
isissi <isissi_525@hotmail.com>
2022-06-29 19:21:02 -0600

    add cartItemCount to cart context

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const addCartItem = (cartItems, productsToAdd) => {
   const existingCartItem = cartItems.find(cartItem => cartItem.id === productsToAdd.id); 
@@ -18,18 +18,25 @@ export const CartContext = createContext({
   isCartOpen: false,
   setIsOpen: () => {},
   cartItems: [], 
-  addItemToCart: () => {}
+  addItemToCart: () => {}, 
+  cartItemCount: 0
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartItemCount, setCartItemCount] = useState(0);
+
+  useEffect(() => {
+    const count = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0); 
+    setCartItemCount(count);
+  }, [cartItems])
 
   const addItemToCart = (productsToAdd) => {
     setCartItems(addCartItem(cartItems, productsToAdd));
   }
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartItemCount };
 
   return (
     <CartContext.Provider value={value}>{children}</CartContext.Provider>
