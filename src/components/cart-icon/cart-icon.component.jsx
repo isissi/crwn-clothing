dbe7ed0fcc69c516625ba75cec8946c225f36993
isissi <isissi_525@hotmail.com>
2022-06-29 19:21:56 -0600

    add total count to cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,14 +6,14 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss'
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartItemCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className='cart-icon-container'>
-      <ShoppingIcon className='shopping-icon' onClick={toggleIsCartOpen} />
-      <span className='item-count'>0</span>
+    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{cartItemCount}</span>
     </div>
   )
 }
