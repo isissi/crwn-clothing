1ccfaca5a98beca5f1514307246cf229cb28ea3d
isissi <isissi_525@hotmail.com>
2022-07-01 17:45:37 -0600

    add checkout to nav bar

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,21 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
+import Checkout from '../../routes/checkout/checkout.component';
 
 import './cart-dropdown.styles.scss'
 
 const CartdDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  }
 
   return (
     <div className='cart-dropdown-container'>
@@ -18,7 +25,7 @@ const CartdDropdown = () => {
         ))}
       </div>
 
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
 
     </div>
   )
