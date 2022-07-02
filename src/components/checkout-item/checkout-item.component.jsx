455e50da03d4ff166b25507993220b0e9da41bd6
isissi <isissi_525@hotmail.com>
2022-07-01 19:14:33 -0600

    add emptyCart function to the remove button

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,12 @@
-import './checkout-item.styles.scss'
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
+import './checkout-item.styles.scss';
 
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
+  const { emtpyItemFromCart } = useContext(CartContext); 
 
   return (
     <div className='checkout-item-container'>
@@ -11,7 +16,7 @@ const CheckoutItem = ({ cartItem }) => {
       <span className='name'>{name}</span>
       <span className='quantity'>{quantity}</span>
       <span className='price'>{price}</span>
-      <div className='remove-button'>&#10005;</div>
+      <div className='remove-button' onClick={() => emtpyItemFromCart(cartItem)}>&#10005;</div>
     </div>
   )
 }
