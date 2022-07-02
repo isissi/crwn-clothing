78e9695d75875798b3e8007497fb2b7895e81432
isissi <isissi_525@hotmail.com>
2022-07-01 18:30:57 -0600

    add function removeCartItem to the - button

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,9 +1,28 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import './checkout.styles.scss'
 
 const Checkout = () => {
+  const { cartItems, addItemToCart, removeItemFromCart } = useContext(CartContext);
+
   return (
     <div>
-
+      {
+        cartItems.map(cartItem => {
+          const {id, name, quantity} = cartItem; 
+          return (
+            <div key ={id}>
+              <h2>{name}</h2>
+              <span>{quantity}</span>
+              <span onClick={() => removeItemFromCart(cartItem)}>-</span>
+              <span onClick={() => addItemToCart(cartItem)}>+</span>
+            </div>
+          )
+        }
+        )
+      }
     </div>
   )
 }
