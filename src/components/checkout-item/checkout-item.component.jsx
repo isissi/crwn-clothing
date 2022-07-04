c9d7dea086af34d42c7d071e730936b8091851f2
isissi <isissi_525@hotmail.com>
2022-07-04 11:19:53 -0600

    add clearItemHandler

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -8,6 +8,8 @@ const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   const { emtpyItemFromCart } = useContext(CartContext); 
 
+  const clearItemHandler = () => emtpyItemFromCart(cartItem);
+
   return (
     <div className='checkout-item-container'>
       <div className='image-container'>
@@ -16,7 +18,7 @@ const CheckoutItem = ({ cartItem }) => {
       <span className='name'>{name}</span>
       <span className='quantity'>{quantity}</span>
       <span className='price'>{price}</span>
-      <div className='remove-button' onClick={() => emtpyItemFromCart(cartItem)}>&#10005;</div>
+      <div className='remove-button' onClick={clearItemHandler}>&#10005;</div>
     </div>
   )
 }
