bc58d5908ae73bf5319cc852fff0c47cc82df0cf
isissi <isissi_525@hotmail.com>
2022-07-10 20:14:11 -0600

    move categoriesProvider to shop component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.contex';
 import { CartProvider } from './contexts/cart.context';
 import reportWebVitals from './reportWebVitals';
 
@@ -13,11 +12,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
     <UserProvider>
-      <CategoriesProvider>
         <CartProvider>
           <App />
         </CartProvider>
-      </CategoriesProvider>
     </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
+import { CategoriesProvider } from "../../contexts/categories.contex";
+
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 
@@ -7,10 +9,12 @@ import './shop.styles.scss'
 
 const Shop = () => {
   return (
-    <Routes>
-      <Route index element={<CategoriesPreview />}/>
-      <Route path=':category' element={<Category />} />
-    </Routes>
+    <CategoriesProvider>
+      <Routes>
+        <Route index element={<CategoriesPreview />}/>
+        <Route path=':category' element={<Category />} />
+      </Routes>
+    </CategoriesProvider>
   )
 }
 
